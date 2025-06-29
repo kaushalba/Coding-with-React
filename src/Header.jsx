a88ce13375f44ffda3e29262f565e48fedd19918
Kaushal <kaushalbandara2002@gmail.com>
2025-06-29 10:16:18 +0530

    updated some files

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ const v1 = "Welcome to build your future";
 
   return(
     <header>
-        <h1>Connect with us to buld your future 44.00</h1>
+        <h1>Connect with us to buld your future  44.00</h1>
         <div>
             <ul>
                 
